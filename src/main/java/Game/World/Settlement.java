ee4f12b256f1becc354e33b8e6dc40f54e084d60
Justin Sandman <supersand24@gmail.com>
2024-12-20 16:22:31 +0000

    Settlements now have basic random names.

--- src/main/java/Game/World/Settlement.java
+++ src/main/java/Game/World/Settlement.java
@@ -4,6 +4,7 @@ import Game.PlayerCharacter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Settlement extends Location {
 
@@ -13,8 +14,17 @@ public class Settlement extends Location {
     List<PlayerCharacter> playersHere = new ArrayList<>();
     
     public Settlement(WorldSpace worldSpace) {
-        this.name = "Unnamed Settlement";
         this.worldSpace = worldSpace;
+        Random random = new Random();
+        String[] potentialNames = {
+            "Noctori City",
+            "Twinleaf Hamlet",
+            "Gregory Pass",
+            "Emberfall Peak",
+            "Spruceheart",
+            "Windmill Hills"
+        };
+        this.name = potentialNames[random.nextInt(potentialNames.length)];
     }
 
 }
